5c407dcee50c67e2d1cf45f741e1638b5eac9335
Alexander Hu <superalexanderhu@gmail.com>
2022-12-02 19:10:39 -0500

    change docker run

--- scripts/docker/kimera_vio_docker.bash
+++ scripts/docker/kimera_vio_docker.bash
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# Allow X server connection
 xhost +local:root
 docker run -it --rm \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
+    --env="DISPLAY=10.31.65.15:0" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    kimera_vio
-# Disallow X server connection
+   --privileged kimera 
 xhost -local:root
+
+# xhost +local:root
+# docker run -it --rm \
+#     --env="DISPLAY" \
+#     --env="QT_X11_NO_MITSHM=1" \
+#     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+#    --privileged kimera 
+# xhost -local:root
+
+# xhost +local:root
+# docker run -it --rm \
+#     --env="DISPLAY=10.31.65.15:0" \    # --env="QT_X11_NO_MITSHM=1" \
+#     kimera # --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+# # Disallow X server connection
+# xhost -local:root
+# --env="QT_X11_NO_MITSHM=1" \
